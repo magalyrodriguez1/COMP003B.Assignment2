3dedb9ac6e2e7863a1a78b6690542beb8f8800c0
magalyrodriguez1 <142467680+magalyrodriguez1@users.noreply.github.com>
2024-04-05 18:29:33 -0700

    Add project files.

---
+++ COMP003B.Assignment2/Controllers/HomeController1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace COMP003B.Assignment2.Controllers
+{
+    public class InfoController : Controller
+    {
+        public IActionResult Index()
+        {
+
+
+            return View();
+        }
+
+     
+    }
+}
